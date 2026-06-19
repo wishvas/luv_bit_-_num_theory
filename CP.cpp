705cab8acc3e9823794355ec95f803c31fca6daa
Vishwas <vishwas.yadav.0816@gmail.com>
2026-06-19 17:45:52 +0530

    Bit by LUV, bit mask worker ques

---
+++ CP.cpp
@@ -0,0 +1,73 @@
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//    cout<<INT_MAX <<endl;
+//    unsigned int a =(1LL<<32) -1 ;
+//    cout<<a<<endl;
+// }
+
+
+
+// SET UNSET BIT
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=9;
+//     int i=3;
+//     if((a & (1<<i)) !=0 ) cout<<"SET BIT"<<endl;
+//     else cout<<"UNSET BIT"<<endl;
+// }
+
+
+
+// UPER LOWER CONVERSION
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//    for(char c='a';c<='e';++c){
+//     cout<<c<<endl;
+//     cout<<bitset<10>(c)<<endl;
+//     cout<<bitset<10>((c & (1<<5)))<<endl;
+//    }
+// }
+
+
+
+
+// BIT MASKING WORKER QUESTION
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main (){
+//         int n;
+//         cin>>n;
+//         vector<int> num_days;
+//         for(int i=0;i<n;++i){
+// cin>>num_days[i];
+//     }
+//     vector<int> a2={2,9,1,7,25,29};
+//     for(int i=0;i<num_days.size();++i){
+//         (0 & (1<<num_days[i]));
+//     }
+//     for(int i=0;i<a2.size();++i){
+//         (0 & (1<<a2[i]));
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+    
+   
